30e835c7c94af3c7afe3e986026b9d817c4ac07d
ibridingpython <ibprofessionalbusinessacc@gmail.com>
2026-03-15 20:04:30 +0700

    Update RandomPassword.py

--- RandomPassword.py
+++ RandomPassword.py
@@ -1,17 +1,33 @@
 from random import choice
 
 # Made by Ibad 15/3/26 18:40
+# Update at 15/03/26 20:00
+# Updated to make sure that it always generates a strong password.
 
 prompt = ("Welcome to password generator!")
 prompt += ("\nEnter 0 to end the program.")
 prompt += ("\nEnter the length of the password: ")
 
-characters = [1,2,3,4,5,6,7,8,9,0,
-              'a','b','c','d','e','f','g',
-              'h','i','j','k','l','m','n','o',
-              'p','q','r','s','t','u','v','w','x','y','z',
-              '!','@','#','$','%','^','&','*','(',')',
-              '-','_','=','+','?']
+lowercase = [
+'a','b','c','d','e','f','g','h','i','j','k','l','m',
+'n','o','p','q','r','s','t','u','v','w','x','y','z'
+]
+
+uppercase = [
+'A','B','C','D','E','F','G','H','I','J','K','L','M',
+'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+]
+
+numbers = [
+'0','1','2','3','4','5','6','7','8','9'
+]
+
+symbols = [
+'!','@','#','$','%','^','&','*','(',')',
+'-','_','=','+','?'
+]
+
+characters = lowercase + uppercase + numbers + symbols
 
 while True:
     move = input(prompt)
@@ -19,8 +35,13 @@ while True:
     
     password = []
     
+    password.append(choice(lowercase))
+    password.append(choice(uppercase))
+    password.append(choice(numbers))
+    password.append(choice(symbols))
+    
     if move != 0:
-        for _ in range(move):
+        for _ in range(move - 4):
             value = choice(characters)
             password.append(value)
             
